a9a62a0ec1217dbbfbc1cc7a5ce264080d07c6d0
Amos Jeffries <yadij@users.noreply.github.com>
2023-01-17 15:55:18 +1300

    Add manuals publishing script (#2)
    * Add manuals publishing script
    .. circa August 2022.
    
    * Rename bin directory to release
    
    Co-authored-by: SquidAdm <noc@lists.squid-cache.org>

---
+++ release/mk-www-manuals.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+#
+# Automatically publish Squid documentation manuals
+# from latest releases of each version.
+#
+
+webpath=`cat ~/.website-path`
+cd ${webpath}/content/Versions/
+
+for v in `ls -1`; do
+	if test "$v" = "." -o "$v" = ".." -o "$v" = "CVS" -o "$v" = "langpack" -o ! -d $v; then
+		continue;
+	fi
+
+	# Manuals only added in v3 and later, but v3 has sub-versions
+	# avoid complications by only updating v4 and later documentation
+	if test "$v" = "v1" -o "$v" = "v2" -o "$v" = "v3"; then
+		continue;
+	fi
+
+	cd $v
+	version=`echo "$v" | sed s/v//`
+
+	# Expand the cfgman and manuals tarballs into their respective directories
+	cfgmantar=`ls -t1 squid-${version}.*-*-cfgman.tar.gz | grep -v snapshot | head -n 1`
+	cd cfgman && tar -xzf ../${cfgmantar}
+	cd ..
+	mantar=`ls -t1 squid-${version}.*-*-manuals.tar.gz | grep -v snapshot | head -n 1`
+	cd manuals && tar -xzf ../${mantar}
+	cd ..
+
+	# Map the Manuals HTML to squid-cache.org .dyn pages
+	echo "<h2>Squid Tools and Helper Manual Pages</h2>" >manuals/index.dyn.tmp
+	echo "<ul>" >>manuals/index.dyn.tmp
+	for f in `ls -1 manuals/*.html` ; do
+		fn=`echo "${f}" | sed s/.1.html// | sed s/.8.html//`
+		fl=`echo "${fn}" | sed s%manuals/%%`
+		cat ${f} | sed \
+			-e 's%<!.*%%' \
+			-e 's%.http://www.w3.org/TR/html4/loose.dtd.*%%' \
+			-e 's%<meta.*%%' \
+			-e 's%<title.*%%' \
+			-e 's%<style.*%%' -e 's%</style>%%' \
+			-e 's%^\ \ .*margin-top.*%%' \
+			-e 's%<head>%%' -e 's%</head>%%' \
+			-e 's%<body>%%' -e 's%</body>%%' \
+			-e 's%<html>%%' -e 's%</html>%%' \
+			-e 's%<br>%<br\ />%' \
+			-e 's%<hr>%<hr\ />%' \
+				>${fn}.dyn.tmp
+		mv -f ${fn}.dyn.tmp ${fn}.dyn
+		touch -r ${f} ${fn}.dyn
+		ln -s ${fl}.html ${fn} 2>/dev/null || true
+		echo "<li><a href=\"${fn}.html\">${fn}</a></li>" | sed -e 's%manuals/%%g' >>manuals/index.dyn.tmp
+	done
+	echo "</ul>" >>manuals/index.dyn.tmp
+	mv -f manuals/index.dyn.tmp manuals/index.dyn
+
+	# leave the sub-version directory
+	cd ..
+done
