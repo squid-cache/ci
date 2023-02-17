9274b0ca65d6117e7e2d87945bdaf0b1edf801e4
SquidAdm <noc@lists.squid-cache.org>
2023-02-17 01:24:01 +0000

    Reviewer requested change

--- release/mk-release-snapshots.sh
+++ release/mk-release-snapshots.sh
@@ -9,23 +9,23 @@
 SQUID_RELEASE=`ls -1 $SQUID_VCS_PATH | cut -d- -f2 | sort -h | tail -n 1`
 
 # Hostname of CI build server
-SQUID_CI_SERVER=buildmaster.squid-cache.org
+SQUID_BUILD_SERVER=buildmaster.squid-cache.org
 
 # file Jenkins uses to assemble the list of snapshot files
 outfile=HEAD.out
 
 ver=$SQUID_RELEASE
-(rsync -rcz --delete-delay rsync://$SQUID_CI_SERVER/snapshots-head/squid-${ver}* $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null &&
-rsync -rcz --delete-delay rsync://$SQUID_CI_SERVER/snapshots-head/$outfile $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null
+(rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-head/squid-${ver}* $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null &&
+rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-head/$outfile $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null
 ) ||
 	echo "rsync pull squid-$ver code snapshots from BuildFarm failed"
 
 ver=$(( $ver - 1 ))
-(rsync -rcz --delete-delay rsync://$SQUID_CI_SERVER/snapshots-latest/squid-${ver}* $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null &&
-rsync -rcz --delete-delay rsync://$SQUID_CI_SERVER/snapshots-latest/$outfile $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null ) ||
+(rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-latest/squid-${ver}* $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null &&
+rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-latest/$outfile $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null ) ||
 	echo "rsync pull squid-$ver code snapshots from BuildFarm failed"
 
 ver=$(( $ver - 1 ))
-(rsync -rcz --delete-delay rsync://$SQUID_CI_SERVER/snapshots-old/squid-${ver}* $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null &&
-rsync -rcz --delete-delay rsync://$SQUID_CI_SERVER/snapshots-old/$outfile $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null ) ||
+(rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-old/squid-${ver}* $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null &&
+rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-old/$outfile $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null ) ||
 	echo "rsync pull squid-$ver code snapshots from BuildFarm failed"
