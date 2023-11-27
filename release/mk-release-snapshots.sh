17704c0bbe985ec3b4f38d5b94b4a65a7f848424
Francesco Chemolli <5175948+kinkie@users.noreply.github.com>
2023-11-27 11:14:38 +0000

    mk-release-snapshot: use jenkins artifacts (#26)
    * mk-release-snapshot: use jenkins artifacts instead of rsync
    
    * Apply suggestions from code review
    
    Co-authored-by: Amos Jeffries <yadij@users.noreply.github.com>
    
    * use sh instead of bash
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex Rousskov <rousskov@measurement-factory.com>
    
    * Update mk-release-snapshots.sh
    
    * Update release/mk-release-snapshots.sh
    
    Co-authored-by: Alex Rousskov <rousskov@measurement-factory.com>
    
    * Update mk-release-snapshots.sh
    
    * Update mk-release-snapshots.sh
    
    * Update mk-release-snapshots.sh
    
    * Update release/mk-release-snapshots.sh
    
    Co-authored-by: Alex Rousskov <rousskov@measurement-factory.com>
    
    * Update mk-release-snapshots.sh
    
    ---------
    
    Co-authored-by: SquidAdm <noc@lists.squid-cache.org>
    Co-authored-by: Amos Jeffries <yadij@users.noreply.github.com>
    Co-authored-by: Alex Rousskov <rousskov@measurement-factory.com>

--- release/mk-release-snapshots.sh
+++ release/mk-release-snapshots.sh
@@ -9,23 +9,53 @@
 SQUID_RELEASE=`ls -1 $SQUID_VCS_PATH | cut -d- -f2 | sort -h | tail -n 1`
 
 # Hostname of the CI server where snapshots are built
-SQUID_BUILD_SERVER=buildmaster.squid-cache.org
+SQUID_BUILD_SERVER=build.squid-cache.org
 
 # file Jenkins uses to assemble the list of snapshot files
 outfile=HEAD.out
 
+# arguments:
+# jenkins job name
+# destination directory
+get_artifacts() {
+	local job=$1
+	local outdir=$2
+	local artifactsUrl="https://$SQUID_BUILD_SERVER/job/${job}/lastSuccessfulBuild/artifact/artifacts/*zip*/artifacts.zip"
+	rm -f artifacts.zip >/dev/null 2>&1
+	if ! wget --quiet --ca-cert=/etc/ssl/certs/ISRG_Root_X1.pem "$artifactsUrl" ; then
+		echo "Failed to download artifacts from $artifactsUrl" 1>&2
+		return 1
+	fi
+ 	rm -r artifacts >/dev/null 2>&1
+ 	if test -e artifacts ; then
+ 	    rm -r artifacts
+ 	    echo "Failed to cleanup stale `pwd`/artifacts" 1>&2
+ 	    return 1
+ 	fi
+	if ! unzip -u -o -qq artifacts.zip ; then
+		echo "Failed to extract files from ${job}:artifacts.zip" 1>&2
+		rm -rf artifacts >/dev/null 2>&1
+  		return 1
+ 	fi
+	if ! test -d artifacts ; then
+		echo "${job}:artifacts.zip does not contain an 'artifacts/' directory" 1>&2
+		rm -d `unzip -Z1 artifacts.zip`
+		rm -f artifacts.zip
+		return 1
+	fi
+	rm artifacts.zip
+	mv artifacts/* $outdir || ( rm -rf artifacts ; return 1 )
+	rm -rf artifacts
+	return 0
+}
+
+cd /var/tmp || exit $?
+
 ver=$SQUID_RELEASE
-(rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-head/squid-${ver}* $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null &&
-rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-head/$outfile $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null
-) ||
-	echo "rsync pull squid-$ver code snapshots from BuildFarm failed"
+get_artifacts website-tarballs-head $SQUID_WWW_PATH/content/Versions/v$ver/
 
 ver=$(( $ver - 1 ))
-(rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-latest/squid-${ver}* $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null &&
-rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-latest/$outfile $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null ) ||
-	echo "rsync pull squid-$ver code snapshots from BuildFarm failed"
+get_artifacts website-tarballs-latest $SQUID_WWW_PATH/content/Versions/v$ver/
 
 ver=$(( $ver - 1 ))
-(rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-old/squid-${ver}* $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null &&
-rsync -rcz --delete-delay rsync://$SQUID_BUILD_SERVER/snapshots-old/$outfile $SQUID_WWW_PATH/content/Versions/v$ver/ 2>/dev/null ) ||
-	echo "rsync pull squid-$ver code snapshots from BuildFarm failed"
+get_artifacts website-tarballs-old $SQUID_WWW_PATH/content/Versions/v$ver/
