999000ba02e07b9cb933b2e5e2cc46856f23eb02
SquidAdm <noc@lists.squid-cache.org>
2025-02-06 10:49:18 +0000

    Add mk-download-page-from-github.py
    It's meant to be called from crontab, it queries github, it
    outputs some php varibles to
    /srv/www/master.squid-cache.org/public_html/content/Versions/github.php.inc
    which is then included by index.dyn to fill in the /Versions/ www page

---
+++ release/mk-download-page-from-github.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+SUPPORTED_VERSIONS=[6]
+
+from subprocess import check_output
+import json
+
+# get releases. Rely that they are in release order
+releases = json.loads(
+    check_output(
+        'gh -R squid-cache/squid release list --exclude-drafts -L 3000 --json name,tagName,createdAt,isPrerelease'.split(" ")
+    )
+)
+
+major_releases=sorted(list(set(map(lambda x: x['tagName'].split('_')[1], releases))))
+major_releases.remove('RELEASES')
+major_releases=sorted(map(int,major_releases),reverse=True)
+
+def render_release(release):
+    releaseDate=release['createdAt'].split('T')[0]
+    releaseUrl=f"https://github.com/squid-cache/squid/releases/tag/{release['tagName']}"
+    return f"<li><a href='{releaseUrl}'>{release['name']}</a> (released {releaseDate})"
+
+print("<?php")
+print("$supported_versions=<<<EOF")
+for r in SUPPORTED_VERSIONS:
+    for release in releases:
+        if release['tagName'].split('_')[1] == str(r):
+            print(render_release(release))
+            break
+print("EOF;")
+
+print("$older_versions=<<<EOF")
+for r in major_releases:
+    if r in SUPPORTED_VERSIONS:
+        continue
+    for release in releases:
+        if release['tagName'].split('_')[1] == str(r):
+            print(render_release(release))
+            break
+print("EOF;")
+print("?>")
