e7701af3f79d176cd5b8e5e88a28aaae1cead118
Amos Jeffries <amosjeffries@squid-cache.org>
2026-01-07 20:54:49 +1300

    Remove group-PRs from backport process

--- maintenance/code-backports.sh
+++ maintenance/code-backports.sh
@@ -7,6 +7,7 @@ verpath="$1"
 version=`basename "$verpath" | sed s/squid-//`
 vnext=$(( $version + 1 ))
 srcbranch=v$vnext
+portLabel="backport-to-v$version"
 
 cd $verpath
 
@@ -43,42 +44,63 @@ abortAndContinue ()
   git $1 --abort 2>&1 >/dev/null || true
 }
 
+makeBranchOrExit ()
+{
+  # systemic error if this fails. abort everything.
+  git checkout $beQuiet -b $1 || exit 1
+}
+
+dropBranch ()
+{
+  git checkout $beQuiet main || exit 1
+  git branch $beQuiet -dD $1 2>/dev/null
+}
+
+dropLabel ()
+{
+  gh pr edit --repo squid-cache/squid $1 --remove-label $portLabel &&
+  gh pr comment --repo squid-cache/squid $1 --body "queued for backport to v$version"
+}
+
+createPortPr ()
+{
+  portBranch="$1"
+
+  # Create a PR to merge (if needed)
+  if test `git diff --shortstat github/$srcbranch $portBranch 2>/dev/null | wc -l` -ne 0 ; then
+    git push $beQuiet -u origin +$portBranch >/dev/null || exit 1
+    # skip if there is an existing PR already open awaiting merge
+    prlist=`gh pr list -L 1 --repo squid-cache/squid --head $portBranch | wc -l`
+    if test "$prlist" -eq 0 ; then
+      gh pr create --repo squid-cache/squid --base v$version --title "v$version Next Backports" --body "" || true
+      dropLabel $prnum
+    fi
+  fi
+}
+
 # Prepare backports branch for updates
-git checkout $beQuiet v$version-next-backports 2>/dev/null &&
+git checkout $beQuiet v$version 2>/dev/null &&
   gitCleanWorkspace &&
     git fetch $beQuiet --all &&
-    git pull $beQuiet origin v$version-next-backports &&
-    git rebase origin/v$version 2>/dev/null &&
-    git push -u origin +v$version-next-backports || abortAndExit rebase
+    git pull $beQuiet || exit 1
 
 # Find backports to attempt:
-prlist=`gh pr list -L 1 --repo squid-cache/squid --state closed --label backport-to-v$version | wc -l`
+prlist=`gh pr list -L 1 --repo squid-cache/squid --state closed --label $portLabel | wc -l`
 if test "$prlist" -ne 0; then
-  gh pr list --repo squid-cache/squid --state closed --label backport-to-v$version | while read prnum text; do
+  gh pr list --repo squid-cache/squid --state closed --label $portLabel | while read prnum text; do
     # find a commit in $srcbranch with " (#$prnum)" subject suffix
     git log --oneline github/$srcbranch --grep=" (#$prnum)\$" | while read sha rest; do
-      msg=`git cherry-pick $beQuiet $sha 2>&1 || abortAndContinue cherry-pick`
+      portBranch="v$version-backport-pr$prnum"
+      makeBranchOrExit $portBranch
+      msg=`git cherry-pick $beQuiet $sha 2>&1 || (abortAndContinue cherry-pick ; dropBranch $portBranch)`
       if ! test -z "$msg" ; then
         echo "$msg" | grep -E "^error:"
         unlabel=`echo "$msg" | grep -E "The.previous.cherry-pick.is.now.empty"`
-        if ! test -z "$unlabel" ; then
-            gh pr edit --repo squid-cache/squid $prnum --remove-label backport-to-v$version &&
-            gh pr comment --repo squid-cache/squid $prnum --body "queued for backport to v$version"
-        fi
+        test -z "$unlabel" && (createPortPr $portBranch || exit 1)
+        test -z "$unlabel" || dropLabel $prnum
       fi
       gitCleanWorkspace
     done
   done
   git push $beQuiet 2>&1 >/dev/null || true
-
-  # Create a PR to merge (if needed)
-  if test `git diff --shortstat github/$srcbranch v$version-next-backports 2>/dev/null | wc -l` -ne 0 ; then
-    git push $beQuiet -u origin +v$version-next-backports >/dev/null || exit 1
-    # skip if there is an existing PR already open awaiting merge
-    prlist=`gh pr list -L 1 --repo squid-cache/squid --head v$version-next-backports | wc -l`
-    if test "$prlist" -eq 0 ; then
-      gh pr create --repo squid-cache/squid --base v$version --title "v$version Next Backports" --body "" || true
-    fi
-  fi
-
 fi
